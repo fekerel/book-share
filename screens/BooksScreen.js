1139881ff188915c56761a0a4d0c74868c09ceb2
fe <fekerel@etu.edu.tr>
2024-11-05 01:50:06 +0300

    some styling

--- screens/BooksScreen.js
+++ screens/BooksScreen.js
@@ -25,27 +25,17 @@ const BooksScreen = () => {
     fetchBooks();
   }, []);
 
-  const renderBookCard = ({ item }) => {
-    console.log(`Image URL: ${BACKEND_URL}${item.image}`); // Bu satırı ekleyin
-    return (
-      <TouchableOpacity
-        style={styles.card}
-        onPress={() => navigation.navigate('BookDetails', { bookId: item.id })}
-      >
-        <Image
-            source={{ uri: `${BACKEND_URL}${item.image}` }}
-            //style={styles.bookImage}
-            style={{width: 50, height: 50}}
-            resizeMode="cover" // veya "contain", "stretch" olarak deneyebilirsiniz
-        />
-
-        <Text style={styles.bookTitle}>{item.title}</Text>
-        <Text style={styles.bookLocation}>{item.location}</Text>
-      </TouchableOpacity>
-    );
-  };
+  const renderBookCard = ({ item }) => (
+    <TouchableOpacity
+      style={styles.card}
+      onPress={() => navigation.navigate('BookDetails', { bookId: item.id })}
+    >
+      <Image source={{ uri: `${BACKEND_URL}${item.image}` }} style={styles.bookImage} />
+      <Text style={styles.bookTitle}>{item.title}</Text>
+      <Text style={styles.bookLocation}>{item.location}</Text>
+    </TouchableOpacity>
+  );
   
-
   if (loading) {
     return <ActivityIndicator size="large" color="#0000ff" />;
   }
@@ -63,7 +53,50 @@ const BooksScreen = () => {
 };
 
 const styles = StyleSheet.create({
-  // Stil ayarları
-});
+    container: {
+      flex: 1,
+      backgroundColor: '#f0f8ff',
+      padding: 16,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    list: {
+      paddingBottom: 16,
+    },
+    card: {
+      backgroundColor: '#fff',
+      borderRadius: 10,
+      padding: 20,
+      marginBottom: 16,
+      width: '90%', // Kart genişliğini ekranın %90'ına yay
+      alignItems: 'center', // İçindeki elemanları yatayda ortalar
+      justifyContent: 'center', // Dikeyde ortalar
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.1,
+      shadowRadius: 4,
+      elevation: 3,
+    },
+    bookImage: {
+      width: 150, // Görüntüyü daha büyük yap
+      height: 200,
+      marginBottom: 12, // Alt boşluk ver
+      borderRadius: 8,
+      resizeMode: 'cover', // Görüntüyü çerçeveye sığacak şekilde ayarla
+    },
+    bookTitle: {
+      fontSize: 20, // Başlık için daha büyük font
+      fontWeight: 'bold',
+      marginBottom: 4,
+      color: '#333',
+      textAlign: 'center',
+    },
+    bookLocation: {
+      fontSize: 16, // Konum için daha büyük font
+      color: '#666',
+      textAlign: 'center',
+    },
+  });
+  
 
 export default BooksScreen;
